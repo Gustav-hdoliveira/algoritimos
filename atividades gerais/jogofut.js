2c5b36db8d69509354a56ab398c8b336cb384d8a
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-03-21 21:49:52 -0300

    Add files via upload

---
+++ atividades gerais/jogofut.js
@@ -0,0 +1,33 @@
+let nomecomple = 'Geovane de Oliveira'
+let tipodejog = 'domestico'
+let etapadojog = 'semi-final'
+let categoria = 3
+quantidade = 1
+preçosemificat3 = 550
+preçofinalca3 =880
+preço3lugarcat3 = 330
+preçosemificat1 = 1320
+preçofinalca1 = 1980
+preço3lugarcat1 = 660
+preçosemificat2 = 880
+preço3lugarcat2 = 440
+preçofinalca2 = 1320
+preçosemificat4 = 220
+preço3lugarcat4 = 170
+preçofinalca4 = 330
+switch (etapadojog) {
+    case 'semi-final':
+        switch (categoria) {
+            case 1:
+                
+                break;
+                case 2:
+        
+            default:
+                break;
+        }
+        break;
+
+    default:
+        break;
+}
